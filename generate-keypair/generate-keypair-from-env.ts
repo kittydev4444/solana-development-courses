0a90b8560455594da2942ac4f9faccc4f7700492
Foursqweez <jeerasak.code@gmail.com>
2025-08-05 16:06:53 +0700

    feat: get keypair from .env

---
+++ generate-keypair/generate-keypair-from-env.ts
@@ -0,0 +1,8 @@
+import { getKeypairFromEnvironment } from '@solana-developers/helpers'
+import 'dotenv/config'
+
+const keypair = getKeypairFromEnvironment('SECRET_KEY')
+
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`
+)
